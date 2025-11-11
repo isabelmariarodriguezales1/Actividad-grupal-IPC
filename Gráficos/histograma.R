5a24ad2bac5957fdfb2ff3c27f259be4a58ef145
irenesales <95928990+irenesales@users.noreply.github.com>
2025-11-11 18:05:21 +0100

    Update histograma.R
    Añadido código de R a histograma.R

--- Gráficos/histograma.R
+++ Gráficos/histograma.R
@@ -1,1 +1,27 @@
+rm(list=ls())
+setwd("C:/Users/master/estadistica y R/tema 3")
+
+
+f <- list.files(pattern="^Dataset.*genes\\.csv$", ignore.case=TRUE)[1]
+df <- read.csv(f, fileEncoding="UTF-8-BOM", check.names=FALSE)
+
+library(ggplot2)
+library(dplyr)
+library(pheatmap)
+
+
+
+#### ---- Histogramas ---- ####
+ggplot(df, aes(x = edad, fill = sexo)) +
+  geom_histogram(color = "black", position = "dodge", bins = 20) +
+  labs(
+    title = "Distribución de la Edad",
+    x = "Edad (años)",
+    y = "Frecuencia"
+  ) +
+  scale_x_continuous(breaks = seq(20, 100, by = 3)) + # Etiquetas cada 3 años
+  scale_fill_manual(values = c("purple", "seagreen3")) + # Morado y verde
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+
 
